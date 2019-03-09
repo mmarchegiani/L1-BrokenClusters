eaeb5cf175096480a1c1c9f68130db123586694e
Matteo Marchegiani (ETHZ) [m.marchegiani] <m.marchegiani@t3ui02.psi.ch>
2019-03-09 20:42:52 +0100

    Bash script to iterate over all .root files

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+python3.6 explore_ntuples.py ../ntuplesPixel/debugHits_MC_ZMM_CluCenteredFull_BIS.root -o
+python3.6 explore_ntuples.py ../ntuplesPixel/debugHits_MC_ZMM_CluCentered_BIS.root -o
+python3.6 explore_ntuples.py ../ntuplesPixel/debugHits_MC_ZMM_CluRefFull_BIS.root -o
+python3.6 explore_ntuples.py ../ntuplesPixel/debugHits_MC_ZMM_CluRef_BIS.root -o
