06d376b461a24e00e90ca3585abdc21e079ac5ea
mmarchegiani <matmarcheg@gmail.com>
2019-03-26 09:06:26 +0100

    Tuning script

--- splitlib.py
+++ splitlib.py
@@ -1,4 +1,5 @@
 import sys, os, pandas
+import shutil
 import uproot
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,23 +7,42 @@ import numpy as np
 # Function to plot the histograms of #clusters(variable) where variable is called 'varname'
 def splitprob(df_full, df_broken, bins=100, varname="", output=True, plot_dir="./"):
 	# To complete: evaluate nfull, nbroken directly from data instead of passing them as parameters
-	index_list = df_grid75.index.values		# List of indices of selected data
+	index_list = df_broken.index.values		# List of indices of selected data
 	nfull = df_full['cols'][index_list[0]]
 	nbroken = df_full['size'][index_list[0]]
 	nrows = df_full['rows'][index_list[0]]
+
+	if nrows < 10:
+		rowname = "rows0" + str(nrows)
+	else:
+		rowname = "rows" + str(nrows)
+
+	main_dir = plot_dir[:]
+	plot_dir = plot_dir + rowname + "/"
+	plt.rcParams['agg.path.chunksize'] = 10000
+
+	if not os.path.exists(plot_dir):
+		os.makedirs(plot_dir)
+
+	shutil.copyfile(main_dir + "index.php", plot_dir + "index.php")
 
 	varlist = ["global_eta", "global_phi", "instaLumi", "bx", "tres"]
 	colors = ["yellow", "blue", "green", "red", "darkturquoise"]
 	colors2 = ["darkorange", "deepskyblue", "lawngreen", "tomato", "cornflowerblue"]
 
-	index = varlist.find(varname)
+	index = varlist.index(varname)
 
 	splitmode = "(" + str(nfull) + "->" + str(nbroken) + ")"
 	print("Plotting histograms " + splitmode + " " + varname + "...")
-	plt.hist(df_full[varname], bins=bins, color=colors[index], ec="black", histtype="stepfilled", log=True, stacked=True)
+	plt.hist(df_full[varname], bins=bins, color=colors[index], ec="black", histtype="stepfilled", log=True, stacked=True, label="cols=" + str(nfull))
 
-	plt.hist(df_broken[varname], bins=bins, color=colors2[index], ec="black", histtype="stepfilled", log=True, stacked=True)
+	plt.hist(df_broken[varname], bins=bins, color=colors2[index], ec="black", histtype="stepfilled", log=True, stacked=True, label="broken")
 	plt.title(varname + " cols=" + str(nfull) + " size=" + str(nbroken) + " rows=" + str(nrows) + " stacked")
+	plt.xlabel(varname)
+	plt.ylabel("#clusters")
+	plt.legend(loc="best")
+	if varname == "bx":
+		plt.xlim(1950, 2050)
 	
 	if output == True:
 		plt.savefig(plot_dir + "cols" + str(nfull) + "_size" + str(nbroken) + "_rows" + str(nrows) + "_" + varname + "stacked.png", format="png", dpi=300)
@@ -32,6 +52,10 @@ def splitprob(df_full, df_broken, bins=100, varname="", output=True, plot_dir=".
 	
 	n_full, bins_full, patches_full = plt.hist(df_full[varname], bins=bins, color=colors[index], ec="black", histtype="stepfilled")
 	plt.title(varname + " cols=" + str(nfull) + " rows=" + str(nrows))
+	plt.xlabel(varname)
+	plt.ylabel("#clusters")
+	if varname == "bx":
+		plt.xlim(1950, 2050)
 
 	if output == True:
 		plt.savefig(plot_dir + "cols" + str(nfull) + "_rows" + str(nrows) + "_" + varname + ".png", format="png", dpi=300)
@@ -41,6 +65,10 @@ def splitprob(df_full, df_broken, bins=100, varname="", output=True, plot_dir=".
 
 	n_broken, bins_broken, patches_broken = plt.hist(df_broken[varname], bins=bins, color=colors2[index], ec="black", histtype="stepfilled")
 	plt.title(varname + " cols=" + str(nfull) + " size=" + str(nbroken) + " rows=" + str(nrows))
+	plt.xlabel(varname)
+	plt.ylabel("#clusters")
+	if varname == "bx":
+		plt.xlim(1950, 2050)
 
 	if output == True:
 		plt.savefig(plot_dir + "cols" + str(nfull) + "_size" + str(nbroken) + "_rows" + str(nrows) + "_" + varname + ".png", format="png", dpi=300)
@@ -54,27 +82,32 @@ def splitprob(df_full, df_broken, bins=100, varname="", output=True, plot_dir=".
 	bin_size = bins_broken[1] - bins_broken[0]
 	bins_broken = np.linspace(start=bins_broken[0] + 0.5*bin_size, stop=bins_broken[-2] + 0.5*bin_size, num=len(bins_broken) - 1, endpoint=True)
 
-	mylist = range(len(bins_broken))
-	for i in mylist:
-		if n_full[i] == 0:
-			bins_broken = np.delete(bins_broken, i)
-			n_broken = np.delete(n_broken, i)
-			bins_full = np.delete(bins_full, i)
-			n_full = np.delete(n_full, i)
+	#n = len(n_full)
+	#mylist = range(n)
+	#for i in mylist:
+	for item in n_full:
+		if item == 0:
+			prob.append(np.nan)
 		else:
-			prob.append(n_broken[i]/n_full[i])
+			i = list(n_full).index(item)
+			prob.append(n_broken[i]/item)
 
 	#bins_broken = np.delete(bins_broken, -1)
 	#plt.scatter(bins_broken, prob, marker='.', color='blue', s=3)
 	sigma = []
 	for i in range(len(prob)):
-		sigma.append(float(prob[i]*np.sqrt(1./n_broken[i] + 1./n_full[i])))		# We propagate the error on the ratio assuming poissonian uncertainties
+		if ((n_broken[i] == 0) | (n_full[i] == 0)):
+			sigma.append(np.nan)
+		else:
+				sigma.append(float(prob[i]*np.sqrt(1./n_broken[i] + 1./n_full[i])))		# We propagate the error on the ratio assuming poissonian uncertainties
 
 	plt.errorbar(bins_broken, prob, yerr=np.array(sigma), fmt='o', ecolor='r', c='r', label="data")
 
+	plt.title("prob splitting " + splitmode + "(rows=" + str(nrows) + ") vs " + varname)
 	plt.xlabel(varname)
 	plt.ylabel("prob" + splitmode)
-	plt.title("prob splitting " + splitmode + "(rows=" + str(nrows) + ") vs " + varname)
+	if varname == "bx":
+		plt.xlim(1950, 2050)
 
 	if output == True:
 		plt.savefig(plot_dir + "prob_rows" + str(nrows) + "_" + varname + str(nfull) + str(nbroken) + ".png", format="png", dpi=300)
@@ -86,7 +119,7 @@ def splitprob(df_full, df_broken, bins=100, varname="", output=True, plot_dir=".
 
 # Function which reads the tree and store the data in 3 dataframes: complete data, cols=nfull data and cols=nfull size=nbroken data
 # The 3 dataframes are returned by the function
-def select(tree, nfull, nbroken, nrows):
+def select_cols(tree, nfull, nbroken):
 	entrystop_ = None
 	if len(tree) > 1.1e8:
 		entrystop_ = 1.1e8
@@ -96,8 +129,14 @@ def select(tree, nfull, nbroken, nrows):
 	df_grid = tree.pandas.df([b'size', b'cols', b'rows', b'x', b'y', b'global_eta', b'global_phi', b'instaLumi', b'bx', b'tres'], entrystop=entrystop_)
 	print(df_grid.head())
 
-	print("Dataframe query...")
-	df_grid_full = df_grid.query('cols == ' + str(nfull) + '& rows == ' + str(nrows))
-	df_grid_broken = df_grid7.query('size == ' + str(nbroken))
+	print("Dataframe query... Selecting cols==" + str(nfull) + " size==" + str(nbroken))
+	df_grid_full = df_grid.query('cols == ' + str(nfull) + '& tres < 5e7')
+	df_grid_broken = df_grid_full.query('size == ' + str(nbroken))
 
 	return df_grid, df_grid_full, df_grid_broken
+
+def select_rows(df, nrows):
+	print("Dataframe query... Selecting rows==" + str(nrows))
+	df_grid_rows = df.query('rows == ' + str(nrows))
+	df_grid_rows.head()
+	return df_grid_rows
--- splitprob.py
+++ splitprob.py
@@ -34,26 +34,14 @@ tree = file[b'a/tree;1']
 #tree.keys()
 print(str(tree.name) + " contains " + str(len(tree)) + " entries")
 
-entrystop_ = None
-if len(tree) > 1.1e8:
-	entrystop_ = 1.1e8
-	print("Dataframe truncated to 1.1e8 events")
-else:
-	print("Dataframe keeped with all the events")
-df_grid = tree.pandas.df([b'size', b'cols', b'rows', b'x', b'y', b'global_eta', b'global_phi', b'instaLumi', b'bx', b'tres'], entrystop=entrystop_)
-print(df_grid.head())
-
-print("Dataframe query...")
-df_grid_full = df_grid.query('cols == 7')
-df_grid_broken = df_grid7.query('size == 5')
-
+df_grid, df_grid_full, df_grid_broken = sp.select_cols(tree, nfull, nbroken)		# Function still to be tested
 varlist = ["global_eta", "global_phi", "instaLumi", "bx", "tres"]
-bins = [100, 100, 20, df_grid_full['bx'].max() - df_grid_full['bx'].min(), 100]
+bins = [100, 100, 20, df_grid_full['bx'].max() - df_grid_full['bx'].min(), 50]
 
 # Iteration over 5 variables and over all possible rows
 rows_max = df_grid['rows'].max()
 rows = range(1, rows_max + 1)
 for nrows in rows:
-	df_grid, df_grid_full, df_grid_broken = select(tree, nfull, nbroken, nrows)		# Function still to be tested
+	df_grid_broken_rows = sp.select_rows(df_grid_broken, nrows)
 	for i in range(len(varlist)):
-		sp.splitprob(df_grid_full, df_grid_broken, 7, 5, bins=bins[i], varname=varlist[i], output=output, plot_dir=plot_dir)
+		sp.splitprob(df_grid_full, df_grid_broken_rows, bins=bins[i], varname=varlist[i], output=output, plot_dir=plot_dir)
